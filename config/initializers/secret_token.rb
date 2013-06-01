317ed96f6cb190e66020e3945b2d168114eb216c
olegrudchenko <oleg.rudchenko@gmail.com>
2013-06-01 22:45:53 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KubsuMicroblog::Application.config.secret_key_base = 'bf6a66423d9997e8f12441503075fc7f52ddfd54e748dda5ca31e9d0d03a465a38abf919777c3869daeb72b71c37acf243808a8826896ea1d177839abac5288f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KubsuMicroblog::Application.config.session_store :cookie_store, key: '_kubsu_microblog_session'
