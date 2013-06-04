e40f47edc1a9bbab5c134c68d972389223c8ff4c
olegrudchenko <oleg.rudchenko@gmail.com>
2013-06-04 10:25:59 +0400

    Finish user edit, update, index, and destroy actions (almost no errors)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
-	def show
+before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+before_action :correct_user,   only: [:edit, :update]
+before_action :admin_user,     only: :destroy
+
+	def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def show
     	@user = User.find(params[:id])
-  	end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Пользователь удален."
+    redirect_to users_url
+  end
 
   def new
   	@user = User.new
@@ -18,10 +32,41 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Информация обновлена"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Пожалуйста, необходимо войти."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
